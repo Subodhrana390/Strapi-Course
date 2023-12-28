f1c486595d4ae929fa59badfdbeda7b5e0af0e46
subodhrana390 <subodhrana390@gmail.com>
2023-12-28 11:03:47 +0545

    first

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+      port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+      database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+      user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+      schema: env("DATABASE_SCHEMA", process.env.DATABASE_SCHEMA), // Not required
+    },
+    debug: false,
+  },
+});
